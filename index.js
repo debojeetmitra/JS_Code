38ed5759c44f54d41983fc7e434df5b974b2320c
Debojeet Mitra <debojeetmitra04@gmail.com>
2025-04-26 21:11:05 +0530

    Variables done

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-console.log(`Hello`);
-console.log(`I love you shreya`);
+// console.log(`Hello`);
+// console.log(`I love you shreya`);
 
-window.alert(`This is an alert`);
-window.alert(`I love you shreya`);
+// window.alert(`This is an alert`);
+// window.alert(`I love you shreya`);
 
-// This is a Comment
+// // This is a Comment
 
-document.getElementById("myH1").textContent = "Hello";
-document.getElementById("myP").textContent = 'I love you Shreya';
+// document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myP").textContent = 'I love you Shreya';
 
 
 
 /* HI */
+
+
+// Understanding the variable
+
+// let x;
+// x = 100;
+
+// console.log(x);
+
+// let age = 24;
+// let gpa = 2.1;
+// console.log(gpa);
+
+// console.log(typeof gpa);
+// console.log(`You are ${age} years old`);
+
+// let firstName = 'Debo123'
+// console.log(typeof firstName);
+
+// console.log(`Your name is ${firstName}`);
+
+
+// Booleans
+
+// let online = true;
+
+// console.log(`I am online ${online}`);
+
+// let forSale = true;
+// console.log(`Is this car for sale ${forSale}`);
+
+let fullName = "debo";
+let age = 25;
+let student = false;
+
+document.getElementById("p1").textContent = `My name is ${fullName}`;
+document.getElementById("p2").textContent = `My age is ${age}`
+document.getElementById("p3").textContent = `Enrolled ${student}`;
+
+
