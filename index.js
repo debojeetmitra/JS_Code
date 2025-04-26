476269abb781b64ed5bf41d0d0aa33fafa4e175e
Debojeet Mitra <debojeetmitra04@gmail.com>
2025-04-26 20:50:15 +0530

    Initial commit - pushed my JavaScript learning code

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log(`Hello`);
+console.log(`I love you shreya`);
+
+window.alert(`This is an alert`);
+window.alert(`I love you shreya`);
+
+// This is a Comment
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = 'I love you Shreya';
+
+
+
+/* HI */
